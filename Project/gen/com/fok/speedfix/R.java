868cf3cf2232f74bf2ad11e5684c02434bb8f923
sirolf2009 <masterflappie@gmail.com>
2014-06-03 12:37:57 +0200

    Work on menu's + notifications

--- Project/gen/com/fok/speedfix/R.java
+++ Project/gen/com/fok/speedfix/R.java
@@ -858,10 +858,13 @@ containing a value of this type.
         public static final int abc_split_action_bar_is_narrow=0x7f060002;
     }
     public static final class color {
-        public static final int abc_search_url_text_holo=0x7f070003;
+        public static final int abc_search_url_text_holo=0x7f070006;
         public static final int abc_search_url_text_normal=0x7f070000;
         public static final int abc_search_url_text_pressed=0x7f070002;
         public static final int abc_search_url_text_selected=0x7f070001;
+        public static final int clrDarkGray=0x7f070004;
+        public static final int clrDarkRed=0x7f070005;
+        public static final int clrWhite=0x7f070003;
     }
     public static final class dimen {
         /**  Default height of an action bar. 
@@ -1069,8 +1072,18 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
+        public static final int logo=0x7f020058;
     }
     public static final class id {
+        public static final int Button01=0x7f050042;
+        public static final int Button02=0x7f050045;
+        public static final int Button03=0x7f050048;
+        public static final int TextView01=0x7f050043;
+        public static final int TextView02=0x7f050041;
+        public static final int TextView03=0x7f050044;
+        public static final int TextView04=0x7f050046;
+        public static final int TextView05=0x7f050047;
+        public static final int TextView06=0x7f050049;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1085,6 +1098,8 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f05003d;
+        public static final int button2=0x7f05003f;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1109,6 +1124,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int relativeLayout1=0x7f05003c;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1126,6 +1142,8 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int textView1=0x7f05003e;
+        public static final int textView2=0x7f050040;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1227,7 +1245,6 @@ containing a value of this type.
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000e;
     }
     public static final class style {
         /** 
--- Project/src/com/fok/speedfix/MainActivity.java
+++ Project/src/com/fok/speedfix/MainActivity.java
@@ -1,15 +1,20 @@
 package com.fok.speedfix;
 
+import com.fok.speedfix.activities.ActivityCompanyList;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+	
+	public static MainActivity instance;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		instance = this;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		ActivityCompanyList.createNotification("Test");
 	}
 }
