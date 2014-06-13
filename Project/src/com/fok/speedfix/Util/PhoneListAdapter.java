d315626955718c2ba4f086ce141979f06cccca32
sirolf2009 <masterflappie@gmail.com>
2014-06-13 10:18:40 +0200

    Better Sign In

--- Project/src/com/fok/speedfix/Util/PhoneListAdapter.java
+++ Project/src/com/fok/speedfix/Util/PhoneListAdapter.java
@@ -1,18 +1,16 @@
 package com.fok.speedfix.util;
 
-import java.io.File;
 import java.util.List;
 
-import com.fok.speedfix.R;
-
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.fok.speedfix.R;
 
 public class PhoneListAdapter extends ArrayAdapter<String> {
 
